e580cbc8e5ecc739606cf2883590daefe04bae9e
Ilson <thuca123@gmail.com>
2020-11-26 10:59:39 -0300

    Compile Fix!

--- modules/mod-boss-announcer/src/mod_boss_announcer.cpp
+++ modules/mod-boss-announcer/src/mod_boss_announcer.cpp
@@ -94,7 +94,7 @@ public:
                 stream << "|CFF" << tag_colour << "|r|cff" << plr_colour << " " << p_name << "|r's Guild |cff" << guild_colour << "" << g_name << "|r acabou de matar |CFF" << boss_colour << "[" << boss_name << "]|r com |cff" << alive_text << "" << Alive_players << " /" << IsNormal << "|r players vivos em " << IsHeroicMode << " mode, com o grupo de |cff" << tag_colour << "Tank: " << Tanks  <<"|r |cff" << guild_colour <<
                     " Healers: "<< Healers << "|r |cff" << boss_colour << " DPS: " << DPS << "|r";
 					
-				if (player->GetMap()->IsHeroic() || boss_name = "The Lich King")
+				if (player->GetMap()->IsHeroic() || boss_name == "The Lich King")
                 sWorld->SendServerMessage(SERVER_MSG_STRING, stream.str().c_str());
 
 
