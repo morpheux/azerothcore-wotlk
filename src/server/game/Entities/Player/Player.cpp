09904e02099819ccd7a05e53937b1084d12a30f6
Ilson <thuca123@gmail.com>
2020-12-24 09:50:10 -0300

    Compile Fix!

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -7524,7 +7524,7 @@ bool Player::RewardHonor(Unit* uVictim, uint32 groupsize, int32 honor, bool awar
             uint32 itemID = sWorld->getIntConfig(CONFIG_PVP_TOKEN_ID);
             int32 count = sWorld->getIntConfig(CONFIG_PVP_TOKEN_COUNT);
 
-            AddItem(itemID, count)
+            AddItem(itemID, count);
         }
     }
 
