61c669c8ecffb3f4b89580369d5caa86c99655cc
Ilson <thuca123@gmail.com>
2020-12-28 15:46:31 -0300

    Moar Fixes!

--- modules/mod-etmaxx-npc/src/etmaxx_npc.cpp
+++ modules/mod-etmaxx-npc/src/etmaxx_npc.cpp
@@ -14,21 +14,25 @@ public:
     // Step 1
     bool OnGossipHello(Player* player, Creature* creature) 
     {
-            if (player->GetUInt32Value(PLAYER_FIELD_LIFETIME_HONORABLE_KILLS) >= 62500 && !player->HasSpell(75614))
-            player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_mount_celestialhorse:50:50|tCelestial Steed", GOSSIP_SENDER_MAIN, 1);
+            player->PlayerTalkClass->ClearMenus();
 
-            player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/INV_BannerPVP_01:50:50|tUNUSED", GOSSIP_SENDER_MAIN, 2);
-            player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Achievement_BG_returnXflags_def_WSG:50:50|tUNUSED2", GOSSIP_SENDER_MAIN, 3);
-            player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/INV_Inscription_Scroll:50:50|tUNUSED3", GOSSIP_SENDER_MAIN, 4);
+            if (player->GetUInt32Value(PLAYER_FIELD_LIFETIME_HONORABLE_KILLS) >= 62500)
+                player->ADD_GOSSIP_ITEM(NULL, "Celestial Steed", GOSSIP_SENDER_MAIN, 1);
+            
+
+            player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/INV_BannerPVP_01:50:50|tCelestial Steed", GOSSIP_SENDER_MAIN, 1);
+            player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_mount_celestialhorse:50:50|tUNUSED2", GOSSIP_SENDER_MAIN, 3);
+            //player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/INV_Inscription_Scroll:50:50|tUNUSED3", GOSSIP_SENDER_MAIN, 4);
             
 			player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, creature->GetGUID());
 
         return true;
     }
 
-    bool OnGossipSelect(Player* player, Creature* creature, uint32 sender, uint32 action)
+    void OnGossipSelect(Player* player, Item* /*item*/, uint32 /*sender*/, uint32 action) override
     {
         player->PlayerTalkClass->ClearMenus();
+
         switch (action)
         {
         case 1:
@@ -36,9 +40,6 @@ public:
             ChatHandler(player->GetSession()).PSendSysMessage("CHAT OUTPUT: Please log out for race change.");
             break;
 		}
-		
-		player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, creature->GetGUID());
-        return true;
 	}
 };
 
