66e82e19dcc06c1eec3c56bd1198dd027469cca4
Ilson <thuca123@gmail.com>
2020-12-28 16:05:02 -0300

    Compile Fix!

--- modules/mod-etmaxx-npc/src/etmaxx_npc.cpp
+++ modules/mod-etmaxx-npc/src/etmaxx_npc.cpp
@@ -16,7 +16,7 @@ public:
     {
             player->PlayerTalkClass->ClearMenus();
 
-            if (player->GetUInt32Value(PLAYER_FIELD_LIFETIME_HONORABLE_KILLS) >= 62500 && !player->HasSpell(75614)
+            if (player->GetUInt32Value(PLAYER_FIELD_LIFETIME_HONORABLE_KILLS) >= 62500 && !player->HasSpell(75614))
                 player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/INV_BannerPVP_01:50:50|tCelestial Steed", GOSSIP_SENDER_MAIN, 1);
             
 			player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, creature->GetGUID());
