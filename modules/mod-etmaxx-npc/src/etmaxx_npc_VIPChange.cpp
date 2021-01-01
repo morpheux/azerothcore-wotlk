e97ad32a45db32f5d466a9a38cd663e2efb8ec0e
Ilson <thuca123@gmail.com>
2021-01-01 20:31:06 -0300

    Item Icons Beta!

--- modules/mod-etmaxx-npc/src/etmaxx_npc_VIPChange.cpp
+++ modules/mod-etmaxx-npc/src/etmaxx_npc_VIPChange.cpp
@@ -37,52 +37,52 @@ public:
             player->ADD_GOSSIP_ITEM(GOSSIP_ACTION_AUCTION, "!!! ATENÇÃO !!! - Este NPC irá DESTRUIR seu item VIP para te dar uma EtMaXx Upgrade Mark", GOSSIP_SENDER_MAIN, 5000);
 
             if (player->HasItemCount(40402,1))
-                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_axe_61:50:50|tVIP Tank Axe", GOSSIP_SENDER_MAIN, 1);
+                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_axe_61:18:18|tVIP Tank Axe", GOSSIP_SENDER_MAIN, 1);
 
             if (player->HasItemCount(47898, 1))
-                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_druid_challangingroar:50:50|tVIP One Hand Axe", GOSSIP_SENDER_MAIN, 2);
+                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_axe_104:18:18|tVIP One Hand Axe", GOSSIP_SENDER_MAIN, 2);
 
             if (player->HasItemCount(34179, 1))
-                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_druid_challangingroar:50:50|tVIP Off-Hand Caster", GOSSIP_SENDER_MAIN, 3);
+                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_offhand_sunwell_d_02:18:18|tVIP Off-Hand Caster", GOSSIP_SENDER_MAIN, 3);
 
             if (player->HasItemCount(34206, 1))
-                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_druid_challangingroar:50:50|tVIP Off-Hand Healer", GOSSIP_SENDER_MAIN, 4);
+                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_offhand_sunwell_d_01:18:18|tVIP Off-Hand Healer", GOSSIP_SENDER_MAIN, 4);
 
             if (player->HasItemCount(49888, 1))
-                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_druid_challangingroar:50:50|tVIP Two-Hand Axe", GOSSIP_SENDER_MAIN, 5);
+                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_axe_113:18:18|tVIP Two-Hand Axe", GOSSIP_SENDER_MAIN, 5);
 
             if (player->HasItemCount(49981, 1))
-                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_druid_challangingroar:50:50|tVIP Crossbow", GOSSIP_SENDER_MAIN, 6);
+                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_weapon_crossbow_38:18:18|tVIP Crossbow", GOSSIP_SENDER_MAIN, 6);
 
             if (player->HasItemCount(51450, 1))
-                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_druid_challangingroar:50:50|tVIP Gun", GOSSIP_SENDER_MAIN, 7);
+                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_weapon_rifle_39:18:18|tVIP Gun", GOSSIP_SENDER_MAIN, 7);
 
             if (player->HasItemCount(51561, 1))
-                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_druid_challangingroar:50:50|tVIP Tank Gun", GOSSIP_SENDER_MAIN, 8);
+                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_weapon_rifle_39:18:18|tVIP Tank Gun", GOSSIP_SENDER_MAIN, 8);
 
             if (player->HasItemCount(50303, 1))
-                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_druid_challangingroar:50:50|tVIP One-Hand Mace", GOSSIP_SENDER_MAIN, 9);
+                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_mace_78:18:18|tVIP One-Hand Mace", GOSSIP_SENDER_MAIN, 9);
 
             if (player->HasItemCount(40395, 1))
-                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_druid_challangingroar:50:50|tVIP Main-Hand Mace Spell", GOSSIP_SENDER_MAIN, 10);
+                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_mace_82:18:18|tVIP Main-Hand Mace Spell", GOSSIP_SENDER_MAIN, 10);
 
             if (player->HasItemCount(42322, 1))
-                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_druid_challangingroar:50:50|tVIP Two-Hand Mace", GOSSIP_SENDER_MAIN, 11);
+                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_hammer_31:18:18|tVIP Two-Hand Mace", GOSSIP_SENDER_MAIN, 11);
 
             if (player->HasItemCount(49301, 1))
-                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_druid_challangingroar:50:50|tVIP Polearm Hunter", GOSSIP_SENDER_MAIN, 12);
+                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_spear_08:18:18|tVIP Polearm Hunter", GOSSIP_SENDER_MAIN, 12);
 
             if (player->HasItemCount(51791, 1))
-                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_druid_challangingroar:50:50|tVIP Shield Spell", GOSSIP_SENDER_MAIN, 13);
+                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_shield_74:18:18|tVIP Shield Spell", GOSSIP_SENDER_MAIN, 13);
 
             if (player->HasItemCount(47079, 1))
-                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_druid_challangingroar:50:50|tVIP Shield Healer", GOSSIP_SENDER_MAIN, 14);
+                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_shield_69:18:18|tVIP Shield Healer", GOSSIP_SENDER_MAIN, 14);
 
             if (player->HasItemCount(50794, 1))
-                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_druid_challangingroar:50:50|tVIP Shield Tank", GOSSIP_SENDER_MAIN, 15);
+                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_shield_73:18:18|tVIP Shield Tank", GOSSIP_SENDER_MAIN, 15);
 
             if (player->HasItemCount(47517, 1))
-                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_druid_challangingroar:50:50|tVIP Main-Hand Sword Spell", GOSSIP_SENDER_MAIN, 16);
+                player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_sword_144:18:18|tVIP Main-Hand Sword Spell", GOSSIP_SENDER_MAIN, 16);
 
 			player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, creature->GetGUID());
 
