16251d75c5f77ac11ac3268a45f97e39a117f977
Glauber Bronzato <glauber.bronzato@gmail.com>
2021-01-01 21:08:17 -0300

    VIP changes

--- modules/mod-etmaxx-npc/src/etmaxx_npc_VIPChange.cpp
+++ modules/mod-etmaxx-npc/src/etmaxx_npc_VIPChange.cpp
@@ -34,7 +34,8 @@ public:
     {
             player->PlayerTalkClass->ClearMenus();
 
-            player->ADD_GOSSIP_ITEM(GOSSIP_ACTION_AUCTION, "!!! ATENÇÃO !!! - Este NPC irá DESTRUIR seu item VIP para te dar uma EtMaXx Upgrade Mark", GOSSIP_SENDER_MAIN, 5000);
+            //player->ADD_GOSSIP_ITEM(GOSSIP_ACTION_AUCTION, "!!! ATENÇÃO !!! - Este NPC irá DESTRUIR seu item VIP para te dar uma EtMaXx Upgrade Mark", GOSSIP_SENDER_MAIN, 5000);
+            player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/Ability_mount_celestialhorse:50:50|tCelestial Steed", GOSSIP_SENDER_MAIN, 5000);
 
             if (player->HasItemCount(40402,1))
                 player->ADD_GOSSIP_ITEM(NULL, "|TInterface/Icons/inv_axe_61:18:18|tVIP Tank Axe", GOSSIP_SENDER_MAIN, 1);
