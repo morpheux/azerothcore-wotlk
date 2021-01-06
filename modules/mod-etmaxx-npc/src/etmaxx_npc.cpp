0cdff2460827665ce7f27dd9d2c53a27c2548d19
Glauber Bronzato <glauber.bronzato@gmail.com>
2021-01-06 17:20:02 -0300

    npc homelander

--- modules/mod-etmaxx-npc/src/etmaxx_npc.cpp
+++ modules/mod-etmaxx-npc/src/etmaxx_npc.cpp
@@ -689,7 +689,7 @@ public:
         return true;
     }
 
-    bool OnGossipSelect(Player* player, Creature* creature, uint32 sender, uint32 /*action*/)
+    bool OnGossipSelect(Player* player, Creature* creature, uint32 sender, uint32 action)
     {
         player->PlayerTalkClass->ClearMenus();
 
