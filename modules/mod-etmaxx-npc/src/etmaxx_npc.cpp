1fcc3d7d11635fe0026643d9eba8e4e03940d45a
Glauber Bronzato <glauber.bronzato@gmail.com>
2021-01-26 09:50:47 -0300

    icon fix

--- modules/mod-etmaxx-npc/src/etmaxx_npc.cpp
+++ modules/mod-etmaxx-npc/src/etmaxx_npc.cpp
@@ -1835,8 +1835,8 @@ public:
     {
         player->PlayerTalkClass->ClearMenus();
 
-        AddGossipItemFor(player, GOSSIP_ICON_MONEY_BAG, "|TInterface/PaperDollInfoFrame/Spellschoolicon7:30:30:-18:0|tResgate sua Recompensa", 100, 0);
-        AddGossipItemFor(player, GOSSIP_ICON_MONEY_BAG, "|TInterface/PaperDollInfoFrame/UI-GearManager-Undo:30:30:-18:0|tUpdate menu", 9999, 0);
+        AddGossipItemFor(player, GOSSIP_ICON_MONEY_BAG, "|TInterface/PaperDollInfoFrame/Spellschoolicon7:35:35:-18:0|tResgate sua Recompensa", 100, 0);
+        AddGossipItemFor(player, GOSSIP_ICON_MONEY_BAG, "|TInterface/PaperDollInfoFrame/UI-GearManager-Undo:35:35:-18:0|tUpdate menu", 9999, 0);
 
         SendGossipMenuFor(player, 800805, creature->GetGUID());
 
