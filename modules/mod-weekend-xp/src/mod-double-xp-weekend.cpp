a451d03fe52eb89ac2468f7a904dd799d4ee6716
Ilson <thuca123@gmail.com>
2021-03-08 11:19:11 -0300

    modules/mod-weekend-xp/src/mod-double-xp-weekend.cpp

--- modules/mod-weekend-xp/src/mod-double-xp-weekend.cpp
+++ modules/mod-weekend-xp/src/mod-double-xp-weekend.cpp
@@ -16,53 +16,18 @@ public:
 
         void OnLogin(Player* player) override
         {
-            // Announce to the player that the XP weekend is happeneing.
-            if (!Enabled)
-                return;
-
             if (now->tm_wday == 5 /*Friday*/ || now->tm_wday == 6 /*Satureday*/ || now->tm_wday == 0/*Sunday*/)
                 ChatHandler(player->GetSession()).PSendSysMessage("Chegou o Final de Semana!!! XP em Dobro para todos!! Rate: X %u", xpAmount);
         }
 
         void OnGiveXP(Player* /*p*/, uint32& amount, Unit* /*victim*/) override
         {
-            if (!Enabled)
-                return;
-
             if (now->tm_wday == 5 /*Friday*/ || now->tm_wday == 6 /*Satureday*/ || now->tm_wday == 0/*Sunday*/)
-                amount *= xpAmount;
+                amount *= 2;
         }
 };
-
-class DoubleXpWeekendConf : public WorldScript
-{
-public:
-    DoubleXpWeekendConf() : WorldScript("DoubleXpConf") {}
-
-    void OnBeforeConfigLoad(bool reload) override
-    {
-        if (!reload) {
-            std::string conf_path = _CONF_DIR;
-            std::string cfg_file = conf_path + "/mod-double-xp-weekend.conf";
-
-#ifdef WIN32
-            cfg_file = "mod-double-xp-weekend.conf";
-#endif
-
-            std::string cfg_def_file = cfg_file + ".dist";
-            sConfigMgr->LoadMore(cfg_def_file.c_str());
-            sConfigMgr->LoadMore(cfg_file.c_str());
-
-            Enabled = sConfigMgr->GetBoolDefault("XPWeekend.Enabled", true);
-            xpAmount = sConfigMgr->GetIntDefault("XPWeekend.xpAmount", 2);
-
-        }
-    }
-};
-
 
 void AdddoublexpScripts()
 {
-    new DoubleXpWeekendConf();
     new DoubleXpWeekend();
 }
