79280fb8cee058ea16b93542837fed7e77f5861f
Ilson <thuca123@gmail.com>
2021-03-08 12:59:22 -0300

    Emblem Transfer e Duel Reset!

--- modules/mod-duel-reset/src/DuelReset.cpp
+++ modules/mod-duel-reset/src/DuelReset.cpp
@@ -30,28 +30,11 @@ class DuelResetScript : public PlayerScript
         // Called when a duel starts (after 3s countdown)
         void OnDuelStart(Player* player1, Player* player2) override
         {
-            // Cooldowns reset
-            if (sConfigMgr->GetBoolDefault("DuelResetCooldowns", true))
-            {
                 // Temporary basic cooldown reset
                 player1->RemoveArenaSpellCooldowns();
                 player2->RemoveArenaSpellCooldowns();
-
-                /* TODO: convert this
-                player1->GetSpellHistory()->SaveCooldownStateBeforeDuel();
-                player2->GetSpellHistory()->SaveCooldownStateBeforeDuel();
-
-                ResetSpellCooldowns(player1, true);
-                ResetSpellCooldowns(player2, true);
-                //*/
-            }
-
-            // Health and mana reset
-            if (sConfigMgr->GetBoolDefault("DuelResetHealthMana", true))
-            {
                 player1->SaveHealthBeforeDuel();
                 player1->SetHealth(player1->GetMaxHealth());
-
                 player2->SaveHealthBeforeDuel();
                 player2->SetHealth(player2->GetMaxHealth());
 
@@ -68,105 +51,34 @@ class DuelResetScript : public PlayerScript
                     player2->SaveManaBeforeDuel();
                     player2->SetPower(POWER_MANA, player2->GetMaxPower(POWER_MANA));
                 }
-            }
         }
 
         // Called when a duel ends
         void OnDuelEnd(Player* winner, Player* loser, DuelCompleteType type) override
         {
-            // do not reset anything if DUEL_INTERRUPTED or DUEL_FLED
-            if (type == DUEL_WON)
+            // Temporary basic cooldown reset
+            winner->SetHealth(winner->GetMaxHealth());
+            loser->SetHealth(loser->GetMaxHealth());
+            winner->RemoveAllSpellCooldown();
+            loser->RemoveAllSpellCooldown();
+
+            // check if player1 class uses mana
+            if (winner->getPowerType() == POWER_MANA || winner->getClass() == CLASS_DRUID)
             {
-                // Cooldown restore
-                if (sConfigMgr->GetBoolDefault("DuelResetCooldowns", true))
-                {
-                    /* TODO: convert this
-                    ResetSpellCooldowns(winner, false);
-                    ResetSpellCooldowns(loser, false);
-
-                    winner->GetSpellHistory()->RestoreCooldownStateAfterDuel();
-                    loser->GetSpellHistory()->RestoreCooldownStateAfterDuel();
-                    //*/
-                }
-
-                // Health and mana restore
-                if (sConfigMgr->GetBoolDefault("DuelResetHealthMana", true))
-                {
-                    winner->RestoreHealthAfterDuel();
-                    loser->RestoreHealthAfterDuel();
-
-                    // check if player1 class uses mana
-                    if (winner->getPowerType() == POWER_MANA || winner->getClass() == CLASS_DRUID)
-                        winner->RestoreManaAfterDuel();
-
-                    // check if player2 class uses mana
-                    if (loser->getPowerType() == POWER_MANA || loser->getClass() == CLASS_DRUID)
-                        loser->RestoreManaAfterDuel();
-                }
-            }
-        }
-
-        /* TODO: convert this
-        static void ResetSpellCooldowns(Player* player, bool onStartDuel)
-        {
-
-            if (onStartDuel)
-            {
-                // remove cooldowns on spells that have < 10 min CD > 30 sec and has no onHold
-                player->GetSpellHistory()->ResetCooldowns([](SpellHistory::CooldownStorageType::iterator itr) -> bool
-                {
-                    SpellHistory::Clock::time_point now = SpellHistory::Clock::now();
-                    uint32 cooldownDuration = itr->second.CooldownEnd > now ? std::chrono::duration_cast<std::chrono::milliseconds>(itr->second.CooldownEnd - now).count() : 0;
-                    SpellInfo const* spellInfo = sSpellMgr->AssertSpellInfo(itr->first);
-                    return spellInfo->RecoveryTime < 10 * MINUTE * IN_MILLISECONDS
-                           && spellInfo->CategoryRecoveryTime < 10 * MINUTE * IN_MILLISECONDS
-                           && !itr->second.OnHold
-                           && cooldownDuration > 0
-                           && ( spellInfo->RecoveryTime - cooldownDuration ) > (MINUTE / 2) * IN_MILLISECONDS
-                           && ( spellInfo->CategoryRecoveryTime - cooldownDuration ) > (MINUTE / 2) * IN_MILLISECONDS;
-                }, true);
-            }
-            else
-            {
-                // remove cooldowns on spells that have < 10 min CD and has no onHold
-                player->GetSpellHistory()->ResetCooldowns([](SpellHistory::CooldownStorageType::iterator itr) -> bool
-                {
-                    SpellInfo const* spellInfo = sSpellMgr->AssertSpellInfo(itr->first);
-                    return spellInfo->RecoveryTime < 10 * MINUTE * IN_MILLISECONDS
-                           && spellInfo->CategoryRecoveryTime < 10 * MINUTE * IN_MILLISECONDS
-                           && !itr->second.OnHold;
-                }, true);
+                winner->SaveManaBeforeDuel();
+                winner->SetPower(POWER_MANA, player1->GetMaxPower(POWER_MANA));
             }
 
-            // pet cooldowns
-            if (Pet* pet = player->GetPet())
-                pet->GetSpellHistory()->ResetAllCooldowns();
-            //
+            // check if player2 class uses mana
+            if (loser->getPowerType() == POWER_MANA || loser->getClass() == CLASS_DRUID)
+            {
+                loser->SaveManaBeforeDuel();
+                loser->SetPower(POWER_MANA, player2->GetMaxPower(POWER_MANA));
+            }
         }
-        */
-};
-
-class DuelResetWorld : public WorldScript
-{
-public:
-    DuelResetWorld() : WorldScript("DuelResetWorld") { }
-
-    void OnBeforeConfigLoad(bool reload) override
-    {
-        if (!reload) {
-            std::string conf_path = _CONF_DIR;
-            std::string cfg_file = conf_path + "/duelreset.conf";
-            std::string cfg_def_file = cfg_file +".dist";
-
-            sConfigMgr->LoadMore(cfg_def_file.c_str());
-
-            sConfigMgr->LoadMore(cfg_file.c_str());
-        }
-    }
 };
 
 void AddSC_DuelReset()
 {
-    new DuelResetWorld();
     new DuelResetScript();
 }
