c2a16cc4a70f6d3e382c748f457e23d5e58392a0
Ilson <thuca123@gmail.com>
2021-03-10 12:25:56 -0300

    Vamos Testar!

--- src/server/game/Handlers/GuildHandler.cpp
+++ src/server/game/Handlers/GuildHandler.cpp
@@ -454,10 +454,7 @@ void WorldSession::HandleGuildBankDepositMoney(WorldPacket& recvData)
             if (Guild* guild = GetPlayer()->GetGuild())
                 guild->HandleMemberDepositMoney(this, money);
 
-    if (_player->GetSession()->GetSecurity() > SEC_PLAYER)
-    {
         sLog->outCommand(_player->GetSession()->GetAccountId(),"GM %s (Account: %u) deposit money (Amount: %u) to guild bank (Guild ID %u)", _player->GetName().c_str(), _player->GetSession()->GetAccountId(), money, GetPlayer()->GetGuild()->GetName().c_str());
-    }
 }
 
 void WorldSession::HandleGuildBankWithdrawMoney(WorldPacket& recvData)
