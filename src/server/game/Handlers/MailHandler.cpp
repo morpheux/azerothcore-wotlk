72917772f108667ad1cf53950a82dc64c1b7921c
Glauber Bronzato <glauber.bronzato@gmail.com>
2021-05-06 12:44:50 -0300

    MAILHANDLER

--- src/server/game/Handlers/MailHandler.cpp
+++ src/server/game/Handlers/MailHandler.cpp
@@ -294,13 +294,8 @@ void WorldSession::HandleSendMail(WorldPacket& recvData)
                 draft.AddItem(item);
             }
 
-            // if item send to character at another account, then apply item delivery delay
+            // if item send to character at another account, then apply item delivery delayy
             needItemDelay = GetAccountId() != rc_account;
-        }
-
-        if (money > 0 && GetPlayer()->GetSession()->GetSecurity() > SEC_PLAYER)
-        {
-            sLog->outCommand(GetPlayer()->GetSession()->GetAccountId(), "GM %s (Account: %u) mail money: " UI64FMTD " to player: %s (Account: %u)", GetPlayer()->GetName().c_str(), GetPlayer()->GetSession()->GetAccountId(), money, receive->GetName().c_str(), receive->GetSession()->GetAccountId());
         }
 
         if( money >= 10 * GOLD )
