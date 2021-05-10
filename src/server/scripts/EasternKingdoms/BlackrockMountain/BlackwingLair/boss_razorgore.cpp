6d7150611dc57e9e5d7556f5995d1c44616ef48b
Ilson Jr <thuca123@gmail.com>
2021-05-10 18:02:04 -0300

    Warning Fix!

--- src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_razorgore.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_razorgore.cpp
@@ -66,7 +66,7 @@ public:
             instance->SetData(DATA_EGG_EVENT, NOT_STARTED);
         }
 
-        void EnterCombat(Unit* victim) override
+        void EnterCombat(Unit* /*victim*/) override
         {
             DoChangePhase();
         }
