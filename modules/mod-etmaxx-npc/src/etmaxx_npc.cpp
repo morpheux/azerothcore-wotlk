f3f3f69510e849e722d5fcc7060f7af3efcacc91
Glauber Bronzato <glauber.bronzato@gmail.com>
2021-05-25 13:44:29 -0300

    warning fix

--- modules/mod-etmaxx-npc/src/etmaxx_npc.cpp
+++ modules/mod-etmaxx-npc/src/etmaxx_npc.cpp
@@ -3181,7 +3181,7 @@ public:
         return true;
     }
 
-    bool OnGossipSelect(Player *player, Creature *creature, uint32 sender, uint32 /*action*/)
+    bool OnGossipSelect(Player *player, Creature /**creature*/, uint32 sender, uint32 /*action*/)
     {
         switch (sender)
         {
