1377cd9e099a406cab7bba422323498a30c73456
Glauber Bronzato <glauber.bronzato@gmail.com>
2021-06-17 03:57:32 -0300

    cast spell

--- modules/mod-etmaxx-npc/src/etmaxx_npc.cpp
+++ modules/mod-etmaxx-npc/src/etmaxx_npc.cpp
@@ -3481,7 +3481,7 @@ public:
 
         case 8:
         {
-            player->CastCustomSpell(7);
+            player->CastSpell(player, 7, false);
             WorldDatabase.PExecute("UPDATE mod_icc_buff SET qty = %u WHERE itemid = %u", qty8+1, itemid8);
             ChatHandler(player->GetSession()).PSendSysMessage("Seu Sacrificio não será em vão");
             CloseGossipMenuFor(player);
